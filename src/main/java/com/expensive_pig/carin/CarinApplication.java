3fea6989519543dc092e5ae8cd8e44f2fa7e42b9
Tanadol Deachprapakorn <putawan-de@hotmail.co.th>
2022-02-08 16:21:54 +0700

    Init project backend, frontend with eslint, prettier

---
+++ src/main/java/com/expensive_pig/carin/CarinApplication.java
@@ -0,0 +1,13 @@
+package com.expensive_pig.carin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/expensive_pig/carin/CarinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expensive_pig.carin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
