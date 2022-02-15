4aecaeb3d1964df1eecb42925c6178a37710283d
min23asdw <min23asdw@outlook.com>
2022-02-15 23:28:33 +0700

    Add: GeneticCodeEvaluatorTest for test

--- src/test/java/com/expensive_pig/carin/evaluator/GeneticCodeEvaluatorTest.java
+++ src/test/java/com/expensive_pig/carin/evaluator/GeneticCodeEvaluatorTest.java
@@ -14,17 +14,45 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GeneticCodeEvaluatorTest {
     private String[] inFileNames = {
-            "src/test/java/com/expensive_pig/carin/evaluator/multipleAssignTest.txt",
-            "src/test/java/com/expensive_pig/carin/evaluator/test.txt",
-            "src/test/java/com/expensive_pig/carin/evaluator/infLoopTest.txt",
-            "src/test/java/com/expensive_pig/carin/evaluator/infLoopNested.txt"
+            "src/test/java/com/expensive_pig/carin/evaluator/input/1_multipleAssignTest.txt",
+            "src/test/java/com/expensive_pig/carin/evaluator/input/2_test.txt",
+            "src/test/java/com/expensive_pig/carin/evaluator/input/3_infLoopTest.txt",
+            "src/test/java/com/expensive_pig/carin/evaluator/input/4_infLoopNestedTest.txt",
+            "src/test/java/com/expensive_pig/carin/evaluator/input/5_sampleTest.txt",
+            "src/test/java/com/expensive_pig/carin/evaluator/input/6_test.txt",
+            "src/test/java/com/expensive_pig/carin/evaluator/input/7_test.txt",
+            "src/test/java/com/expensive_pig/carin/evaluator/input/8_test.txt",
+            "src/test/java/com/expensive_pig/carin/evaluator/input/9_test.txt",
+            "src/test/java/com/expensive_pig/carin/evaluator/input/10_WhileTest.txt",
+            "src/test/java/com/expensive_pig/carin/evaluator/input/11_WhileTest.txt",
+            "src/test/java/com/expensive_pig/carin/evaluator/input/12_BlockStatementTest.txt",
+            "src/test/java/com/expensive_pig/carin/evaluator/input/13_BlockStatementTest.txt",
+            "src/test/java/com/expensive_pig/carin/evaluator/input/14_BlockStatementTest.txt",
+            "src/test/java/com/expensive_pig/carin/evaluator/input/15_OrderTest.txt",
+            "src/test/java/com/expensive_pig/carin/evaluator/input/16_OrderTest.txt"
+
     };
 
     private String[] expectedFileNames = {
-            "src/test/java/com/expensive_pig/carin/evaluator/expectedMultipleAssignTest.txt",
-            "src/test/java/com/expensive_pig/carin/evaluator/expectedTest.txt",
-            "src/test/java/com/expensive_pig/carin/evaluator/empty.txt",
-            "src/test/java/com/expensive_pig/carin/evaluator/empty.txt",
+            "src/test/java/com/expensive_pig/carin/evaluator/expected/1_expectedTest.txt",
+            "src/test/java/com/expensive_pig/carin/evaluator/expected/2_expectedTest.txt",
+            "src/test/java/com/expensive_pig/carin/evaluator/expected/3_expectedTest.txt",
+            "src/test/java/com/expensive_pig/carin/evaluator/expected/4_expectedTest.txt",
+            "src/test/java/com/expensive_pig/carin/evaluator/expected/5_expectedTest.txt",
+            "src/test/java/com/expensive_pig/carin/evaluator/expected/6_expectedSampleTest.txt",
+            "src/test/java/com/expensive_pig/carin/evaluator/expected/7_expectedSampleTest.txt",
+            "src/test/java/com/expensive_pig/carin/evaluator/expected/8_expectedSampleTest.txt",
+            "src/test/java/com/expensive_pig/carin/evaluator/expected/9_expectedSampleTest.txt",
+            "src/test/java/com/expensive_pig/carin/evaluator/expected/10_expectedSampleTest.txt",
+            "src/test/java/com/expensive_pig/carin/evaluator/expected/11_expectedSampleTest.txt",
+            "src/test/java/com/expensive_pig/carin/evaluator/expected/12_expectedSampleTest.txt",
+            "src/test/java/com/expensive_pig/carin/evaluator/expected/13_expectedSampleTest.txt",
+            "src/test/java/com/expensive_pig/carin/evaluator/expected/14_expectedSampleTest.txt",
+            "src/test/java/com/expensive_pig/carin/evaluator/expected/15_expectedSampleTest.txt",
+            "src/test/java/com/expensive_pig/carin/evaluator/expected/16_expectedSampleTest.txt"
+
+
+
     };
 
     @Test
