417013075f4a0cc9ffd0ff671b8d175a32f74d26
min23asdw <min23asdw@outlook.com>
2022-02-18 21:23:12 +0700

    Update: user Virus genetic for InitGame

--- src/main/java/com/expensive_pig/carin/core/InitGame.java
+++ src/main/java/com/expensive_pig/carin/core/InitGame.java
@@ -21,9 +21,20 @@ public class InitGame {
     }
 
     public Program[] InitVirus() throws SyntaxError {
+                /*
+         receive user Virus genetic . which gen user manually upload use String
+         */
+        boolean whichmanuallyupload = false;
+        String userGenetic = "";
+        /*    ...
+         */
         int i = 0;
         for (String gen : geneticloader.ViusGenetic.keySet()) {
-            virus[i] = programeReader.getProgrambyPath(geneticloader.ViusGenetic.get(gen));
+            if (whichmanuallyupload) {
+                virus[i] = programeReader.getProgrambyString(geneticloader.ViusGenetic.get(gen));
+            }else {
+                virus[i] = programeReader.getProgrambyPath(geneticloader.ViusGenetic.get(gen));
+            }
             i++;
         }
         return virus;
@@ -31,23 +42,20 @@ public class InitGame {
 
     public Program[] InitAnti() throws SyntaxError {
         /*
-         receive user genetic . which gen user manually upload use String
+         receive user Anti genetic . which gen user manually upload use String
          */
         boolean whichmanuallyupload = false;
         String userGenetic = "";
         /*    ...
-         *
          */
         int i = 0;
         for (String gen : geneticloader.AntiGenetic.keySet()) {
 
             if (whichmanuallyupload) {
-                anti[i] = programeReader.getProgrambyPath(geneticloader.AntiGenetic.get(gen));
+                anti[i] = programeReader.getProgrambyString(geneticloader.AntiGenetic.get(gen));
             } else {
-                anti[i] = programeReader.getProgrambyString(userGenetic);
+                anti[i] = programeReader.getProgrambyPath(userGenetic);
             }
-
-
             i++;
         }
         return anti;
