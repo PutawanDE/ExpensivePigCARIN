79cf0c8eb041fee877a753acaf88cce80d431e67
Purich <purichseenuallae@gmail.com>
2022-03-04 13:51:34 +0700

    RESPONSIVE ANTIBODY BTN

--- carin-frontend/src/components/selectantigen/SelectAntigen.tsx
+++ carin-frontend/src/components/selectantigen/SelectAntigen.tsx
@@ -11,21 +11,21 @@ function SelectAntigen() {
 
   return (
     <div className="background">
-      <div className="container mx-auto py-10 text-center text-3xl font-bold">
+      <div className="container mx-auto py-10 text-center text-5xl font-bold">
               EDIT GENETIC CODE
       </div>
       <div className="flex flex-row ">
-        <div className="basis-1/3">
+        <div className="basis-1/3 px-20">
           <button className="center"><img src={AGA} alt="" className="geneticbuttonpicture " /></button>
         </div>
-        <div className="basis-1/3">
+        <div className="basis-1/3 px-20">
           <button className="center"><img src={AGB} alt="" className="geneticbuttonpicture " /></button>
         </div>
-        <div className="basis-1/3">
+        <div className="basis-1/3 px-20">
           <button className="center"><img src={AGC} alt="" className="geneticbuttonpicture " /></button>
         </div>
       </div>
-      <div className="right pr-12 pt-4">
+      <div className="right pr-12 pt-3">
         <button className="nextBTN"><img src={NextBTN} alt="" className="geneticbuttonpicture " /></button>
       </div>
     </div>
