15e44e691c3e3b95b8fbf0d7130deb1cda2a2005
Purich <purichseenuallae@gmail.com>
2022-03-14 01:01:07 +0700

    FIX CSS BACKGROUND PROBLEM

--- carin-frontend/src/components/result/AntigenResult.tsx
+++ carin-frontend/src/components/result/AntigenResult.tsx
@@ -3,12 +3,11 @@ import Trophy from "../../elements/result/trophy.gif";
 
 function AntigenResult() {
     return (
-        <div>
+        <div className="backgroundResult">
             <div className="pt-10">
                 <img src={Trophy} className="trophy" />
             </div>
-
-            <div className="textborder text-6xl font-bold py-10">
+            <div className="textborder-pink text-6xl font-bold py-10">
                 THE WINNER IS COVID-19
             </div>
 
