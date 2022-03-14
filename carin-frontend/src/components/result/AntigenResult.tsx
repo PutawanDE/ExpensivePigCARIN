4ba2fe93e975e571593c1f9931f45ee1caf6e5b8
Purich <purichseenuallae@gmail.com>
2022-03-14 18:13:04 +0700

    ADD UI RESULT

--- carin-frontend/src/components/result/AntigenResult.tsx
+++ carin-frontend/src/components/result/AntigenResult.tsx
@@ -3,13 +3,17 @@ import Trophy from "../../elements/result/trophy.gif";
 
 function AntigenResult() {
     return (
-        <div className="backgroundResult">
+        <div className="resultbackground">
             <div className="pt-10">
                 <img src={Trophy} className="trophy" />
             </div>
-            <div className="textborder-pink text-6xl font-bold py-10">
+            <div className="textborder-pink text-5xl font-bold py-10">
                 THE WINNER IS COVID-19
             </div>
+            <button className="center bg-blue-500 hover:bg-blue-700 text-white font-bold py-6 px-10 hover:px-9 rounded-full">
+                Play Again
+            </button>
+
 
         </div>
     );
