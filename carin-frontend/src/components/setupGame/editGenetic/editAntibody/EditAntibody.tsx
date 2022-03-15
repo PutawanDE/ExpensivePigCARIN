9c287297e5edc05fd6779ec403adcea88f92ed85
Purich <purichseenuallae@gmail.com>
2022-03-16 01:49:30 +0700

    ADD ANTIBODY TEXTBOX POPUP AND GRAPHIC

--- carin-frontend/src/components/setupGame/editGenetic/editAntibody/EditAntibody.tsx
+++ carin-frontend/src/components/setupGame/editGenetic/editAntibody/EditAntibody.tsx
@@ -8,7 +8,7 @@ import GeneticForm from '../GeneticForm';
 import AGA from "../../assets/AGA.gif";
 import AGB from "../../assets/AGB.gif";
 import AGC from "../../assets/AGC.gif";
-import NextBTN from "../../assets/NextBTN.png";
+
 
 import '../GeneticForm.css';
 import './EditAntibody.css';
@@ -43,11 +43,22 @@ function EditAntibody() {
     setup.setupData.anti = genetic
   }
 
+  const ConfigForm = (props: any) => {
+    return (
+      <div className="config-popup-box">
+        <div className="config-text-box">
+          <span className="close-icon" onClick={props.handleClose}></span>
+          {props.content}
+        </div>
+      </div>
+    );
+  };
+
 
 
   return (
-    <div className="background">
-      <div className="container mx-auto py-10 text-center text-5xl font-bold">
+    <div className="background backgroundEdit">
+      {/* <div className="container mx-auto py-10 text-center text-5xl font-bold">
         EDIT ANTIBODY GENETIC CODE
       </div>
       <div className="flex flex-row ">
@@ -62,67 +73,92 @@ function EditAntibody() {
         </div>
       </div>
 
-
-      {isOpen && <GeneticForm content={
-      <>
-        {/* <textarea
-          name={selectbox}
-          onChange={handleChange}
-          className='geneticbox'
-          rows={20}
-          cols={150} />
-        <button onClick={handleSave} >Test button</button> */}
-              <div className="py-5">
-        <div className="textboxBackground pt-5 pb-10 ">
-          <div className="center text-2xl font-bold">
-            Game Configuration
-          </div>
-          <form onSubmit={handleChange} className="center py-6">
-            <textarea
-              name={selectbox}
-              onChange={handleChange}
-              rows={15}
-              cols={80}
-              className="fixedtextarea focus:ring ring-pink-500 focus:border-rose-700 
-              focus:outline-none px-4 py-4 border-dashed border-4 border-rose-500 rounded-3xl"
-            />
-          </form>
-          <div className="center">
-            <div className="flex flex-row justify-center">
-              <div className="px-2">
-               
-                  <button   type="submit"
-                    className="bg-green-500 hover:bg-green-700 text-white 
-                font-bold py-2 px-8 rounded-full text-black text-lg"
-                    onClick={handleSave}>
-                    Submit
-                  </button>
-              </div>
-
-              <div className="px-2">
-                <button
-                  className="bg-red-500 hover:bg-red-700 text-white 
-                font-bold py-2 px-8 rounded-full text-black text-lg">
-                  Clear
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-         
-      </div>
-      </>
-      }
-        handleClose={togglePopup}
-      />}
-      <div className="right pr-12 pt-3">
+            <div className="right pr-12 pt-3">
         <Link to="/editVirus">
-
-          {/* save anti-genetic string[]*/}
-
           <button name={"editAntibody"} onClick={save} className="nextBTN"><img src={NextBTN} alt="" className="geneticbuttonpicture " /></button>
         </Link>
+      </div> */}
+
+      <div className="parent flex flex-col justify-center center">
+        <div className="configBorder font-bold text-5xl py-6">
+          EDIT ANTIBODY GENETIC CODE
+        </div>
+        <div className="px-32">
+          <div className="flex flex-row justify-center child">
+            <div className="pr-20">
+              <button onClick={() => togglePopup()}>
+                <img src={AGA} className="selectModeSize " />
+              </button>
+            </div>
+            <div className="pr-20">
+              <button onClick={() => togglePopup()}>
+                <img src={AGB} className="selectModeSize " />
+              </button>
+            </div>
+            <div className="">
+              <button onClick={() => togglePopup()}>
+                <img src={AGC} className="selectModeSize" />
+              </button>
+            </div>
+
+          </div>
+        </div>
+        <div className="py-4">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-10 rounded-full">
+            Done
+          </button>
+        </div>
       </div>
+
+
+      {
+        isOpen && <ConfigForm content={
+          <div className="config-container  flex h-screen w-screen bg-neutral-900/50 z-50  py-20 textboxBackground">
+            <div className="">
+              <div className="flex flex-col pt-5 pb-10 ">
+                <div className="center text-2xl font-bold">
+                  Game Configuration
+                </div>
+                <form onSubmit={handleChange} className="center py-6">
+                  <textarea
+                    name={selectbox}
+                    onChange={handleChange}
+                    rows={15}
+                    cols={80}
+                    className="fixedtextarea focus:ring ring-pink-500 focus:border-rose-700 
+              focus:outline-none px-4 py-4 border-dashed border-4 border-rose-500 rounded-3xl"
+                  />
+                </form>
+                <div className="center">
+                  <div className="flex flex-row justify-center">
+                    <div className="px-2">
+
+                      <button type="submit"
+                        className="bg-green-500 hover:bg-green-700 text-white 
+                font-bold py-2 px-8 rounded-full text-black text-lg"
+                        onClick={handleSave}>
+                        Submit
+                      </button>
+                    </div>
+
+                    <div className="px-2">
+                      <button
+                        className="bg-red-500 hover:bg-red-700 text-white 
+                font-bold py-2 px-8 rounded-full text-black text-lg">
+                        Clear
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        }
+          handleClose={togglePopup}
+        />
+      }
+
 
     </div>
   )
