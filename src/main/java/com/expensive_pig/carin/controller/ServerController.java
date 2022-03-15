90b144377cada1168f26fd5d687e8c55710ce017
Tanadol Deachprapakorn <putawan-de@hotmail.co.th>
2022-03-16 02:49:57 +0700

    Add: handle disconnection, end game thread

--- src/main/java/com/expensive_pig/carin/controller/ServerController.java
+++ src/main/java/com/expensive_pig/carin/controller/ServerController.java
@@ -7,6 +7,7 @@ import com.expensive_pig.carin.game_data.GameStartResp;
 import com.expensive_pig.carin.repository.GameRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -14,10 +15,11 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 @Controller
 @Slf4j
-public class StartGameController {
+public class ServerController {
 
     @Autowired
     private TaskExecutor taskExecutor;
@@ -28,7 +30,7 @@ public class StartGameController {
     private GameRepository gameRepository;
 
     @Autowired
-    public StartGameController(SimpMessagingTemplate template) {
+    public ServerController(SimpMessagingTemplate template) {
         this.template = template;
     }
 
@@ -37,7 +39,7 @@ public class StartGameController {
         log.info("Request to start a game by sessionID: " + sessionId);
         GameStartResp resp;
 
-        if(!gameRepository.containGame(sessionId)) {
+        if (!gameRepository.containGame(sessionId)) {
             startNewGame(sessionId, setup);
         } else {
             resp = new GameStartResp("FAIL", "Game with this id already exist.", null);
@@ -67,5 +69,15 @@ public class StartGameController {
             log.error("Fail to start a game. sessionID: " + sessionId + " msg: " + resp.getMsg());
             template.convertAndSend("/queue/start-" + sessionId, resp);
         }
+    }
+
+    @EventListener
+    public void onDisconnectEvent(SessionDisconnectEvent event) {
+        String id = event.getSessionId();
+        if (gameRepository.containGame(id)) {
+            gameRepository.remove(id).end();
+        }
+
+        log.info("Client with id " + event.getSessionId() + " disconnected", event.getUser());
     }
 }
