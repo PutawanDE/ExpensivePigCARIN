c3ddd668c2ba6c7f688151efe391befcd12b08cb
Purich <purichseenuallae@gmail.com>
2022-03-16 03:53:35 +0700

    ADD COINBAR UI

--- carin-frontend/src/components/gameloop/gameComponents/Credit.tsx
+++ carin-frontend/src/components/gameloop/gameComponents/Credit.tsx
@@ -1,20 +1,20 @@
 import { CreditStore } from "../stores/CreditStore"
 import creditImage from "../assets/credit.png";
- 
+import "./Panel.css"
+
 
 const Credit = () => {
 
   const credit = CreditStore.useState()
- 
- 
+
+
 
   return (
-    <div className="coin">
-          <img className="w-36 icon" src={creditImage }   />
-          <p>{credit.creditData.credit}</p>
- 
-          
-    </div>
+      <div className="coinpanel py-10 px-40">
+        <div className="cointext text-white text-3xl">
+          {credit.creditData.credit}
+        </div>
+      </div>
   )
 }
 
