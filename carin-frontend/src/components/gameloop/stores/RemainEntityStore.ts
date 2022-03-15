5e64f3271bad788681d2029936bf396b1e1ebaa4
min23asdw <min23asdw@outlook.com>
2022-03-15 22:14:35 +0700

    ADD: RemainStore to store remain anti, virus

---
+++ carin-frontend/src/components/gameloop/stores/RemainEntityStore.ts
@@ -0,0 +1,21 @@
+import { Store } from 'pullstate'
+ 
+type RemainProps = {
+    antiRemain: number;
+    virusRemain: number;
+ 
+}
+const defaultRemain: RemainProps = {
+    antiRemain: 0,
+    virusRemain: 0
+}
+
+type RemainStore = {
+  RemainData: RemainProps;
+}
+
+ 
+
+export const RemainStore = new Store<RemainStore>({
+    RemainData: defaultRemain,
+})
